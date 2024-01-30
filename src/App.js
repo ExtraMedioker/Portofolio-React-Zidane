5272607cb2a15aa33ea20d116ffff0bc7e64b539
ExtraMedioker <mzidanabdulkadir@gmail.com>
2024-01-30 11:29:34 +0800

    Less Code

--- src/App.js
+++ src/App.js
@@ -37,34 +37,6 @@ function App() {
     localStorage.setItem("theme", newTheme);
   };
 
-  useEffect(() => {
-    const headerBtn = document.querySelector(".header__bars");
-    const mobileNav = document.querySelector(".mobile-nav");
-    const mobileLinks = document.querySelectorAll(".mobile-nav__link");
-
-    // State
-    let isMobileNavOpen = false;
-
-    headerBtn.addEventListener("click", () => {
-      isMobileNavOpen = !isMobileNavOpen;
-      if (isMobileNavOpen) {
-        mobileNav.style.display = "flex";
-        document.body.style.overflowY = "hidden";
-      } else {
-        mobileNav.style.display = "none";
-        document.body.style.overflowY = "auto";
-      }
-    });
-
-    mobileLinks.forEach((link) => {
-      link.addEventListener("click", () => {
-        isMobileNavOpen = false;
-        mobileNav.style.display = "none";
-        document.body.style.overflowY = "auto";
-      });
-    });
-  }, [])
-
   return (
     <>
       <Header darkMode={toggleTheme} />
